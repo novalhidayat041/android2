3c269f79c20bdaca1d95c2f31a561faf8bb03fb9
novalhidayat041 <mnovalhidayat298@gmail.com>
2022-06-16 10:31:08 +0700

    ACTIVITY LOGIN

---
+++ app/src/main/java/id/ac/polbeng/jasaonline/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package id.ac.polbeng.jasaonline.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.polbeng.jasaonline.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
