933611f7cdd634385115f2f6e184cca856858b23
novalhidayat041 <mnovalhidayat298@gmail.com>
2022-06-16 10:03:10 +0700

    a

---
+++ app/src/main/java/id/ac/polbeng/jasaonline/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package id.ac.polbeng.jasaonline
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import id.ac.polbeng.jasaonline.helpers.Config
+
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, Config.SPLASH_TIME_OUT)
+
+    }
+}
---
+++ app/src/main/java/id/ac/polbeng/jasaonline/helpers/Config.kt
@@ -0,0 +1,7 @@
+package id.ac.polbeng.jasaonline.helpers
+
+class Config {
+    companion object{
+        const val SPLASH_TIME_OUT:Long = 3000
+    }
+}
