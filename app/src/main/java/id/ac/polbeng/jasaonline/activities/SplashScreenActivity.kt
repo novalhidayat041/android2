058f538b196c5afa810f5d6ca586a06901ff8191
novalhidayat041 <mnovalhidayat298@gmail.com>
2022-06-16 10:21:29 +0700

    move to paccage activity

--- app/src/main/java/id/ac/polbeng/jasaonline/activities/SplashScreenActivity.kt
+++ app/src/main/java/id/ac/polbeng/jasaonline/activities/SplashScreenActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import id.ac.polbeng.jasaonline.R
+import id.ac.polbeng.jasaonline.activities.MainActivity
 import id.ac.polbeng.jasaonline.helpers.Config
 
 
